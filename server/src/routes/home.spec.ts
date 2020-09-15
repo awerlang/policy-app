cfb69d97d8b084f89ac0b8ab0530216c556246bc
André Werlang <beppe85@gmail.com>
2020-09-15 17:25:27 -0300

    fix: server tests

--- server/src/routes/home.spec.ts
+++ server/src/routes/home.spec.ts
@@ -3,13 +3,14 @@ import request from 'supertest'
 import app from './home'
 
 describe('GET /', function () {
-    it('respond with html', function (done) {
+    it('redirect to index.html', function (done) {
         request(app)
             .get('/')
             .set('Accept', 'text/html')
-            .expect(200)
+            .expect(302)
             .end(function (err, res) {
                 if (err) return done(err)
+                expect(res.get('location')).toBe('index.html')
                 done()
             })
     })
